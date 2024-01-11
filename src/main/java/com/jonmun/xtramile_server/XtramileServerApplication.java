3a9df9c55ee8f10229f893d66893f52b17b64327
jon-mun <jonathan.edmund@gmail.com>
2024-01-11 16:25:57 +0700

    initialize spring boot application

---
+++ src/main/java/com/jonmun/xtramile_server/XtramileServerApplication.java
@@ -0,0 +1,13 @@
+package com.jonmun.xtramile_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtramileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtramileServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonmun/xtramile_server/XtramileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonmun.xtramile_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtramileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

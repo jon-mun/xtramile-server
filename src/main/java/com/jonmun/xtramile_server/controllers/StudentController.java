5afe337e7465b335fbd49f49117f263e9fac3624
jon-mun <jonathan.edmund@gmail.com>
2024-01-13 17:25:29 +0700

    fix: allow client cross origin request

--- src/main/java/com/jonmun/xtramile_server/controllers/StudentController.java
+++ src/main/java/com/jonmun/xtramile_server/controllers/StudentController.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     private StudentService studentService;
